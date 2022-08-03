b40f6f72335527af4cdf499a7052d58be8740651
Yash Jagtap <jagtapyash2512@gmail.com>
2022-08-03 12:10:47 +0530

    declare struct, mapping

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -1,2 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
+
+contract Web3RSVP {
+    struct CreateEvent {
+        bytes32 eventId;
+        string eventDataCID; // reference to an IPFS hash for storing details like the event’s name and event description
+        address eventOwner;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        uint256 maxCapacity;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+    }
+
+    mapping(bytes32 => CreateEvent) public idToEvent;
+}
